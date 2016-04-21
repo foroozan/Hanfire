923a974fc87fb26e2f7cb3ad5a5b59e26c9f361f
forooz <foroozan.mahdavi@euromoneyplc.com>
2016-04-21 11:53:22 +0100

    first commit

---
+++ HangFire2/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Hangfire;
+using Owin;
+
+namespace HangFire2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(@"Data Source = EULONWL10052\DSI;  initial catalog = Hangfire; Integrated Security = True;");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate(
+                () => Write(),
+                Cron.Hourly);
+        }
+
+        public void Write()
+        {
+            string lines = "First line.\r\nSecond line.\r\nThird line.";
+
+            // Write the string to a file.
+            System.IO.StreamWriter file = new System.IO.StreamWriter("e:\\input.txt");
+            file.WriteLine(lines);
+
+            file.Close();
+        }
+    }
+}
